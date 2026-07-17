f262a7add18756d714c15a98ea0c51983004eaa6
piero.cianciotta <piero.cianciotta@gmail.com>
2026-07-17 08:27:07 +0200

    final

--- build/CMakeFiles/stm32_benchmark.elf.dir/adapters/crypto_x25519.c.obj.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/stm32_benchmark.elf.dir/adapters/crypto_x25519.c.obj: \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/adapters/crypto_x25519.c \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint-gcc.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stddef.h \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/third_party/X25519-Cortex-M4/x25519-cortex-m4.h \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/core/inc/crypto_api.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdbool.h
--- build_iso/CMakeFiles/stm32_benchmark.elf.dir/platforms/stm32_renode/runtime_overhead_probe.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/stm32_benchmark.elf.dir/platforms/stm32_renode/runtime_overhead_probe.c.obj: \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/platforms/stm32_renode/runtime_overhead_probe.c
