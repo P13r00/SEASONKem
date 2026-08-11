314f9b002c1280ab2b8270c9c870158ff6c17529
piero.cianciotta <piero.cianciotta@gmail.com>
2026-08-11 17:25:25 +0200

    Stop tracking build directory

--- build/CMakeFiles/stm32_benchmark.elf.dir/core/src/benchmark_metrics.c.obj.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/stm32_benchmark.elf.dir/core/src/benchmark_metrics.c.obj: \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/core/src/benchmark_metrics.c \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/core/src/benchmark_metrics.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stddef.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint-gcc.h \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/core/inc/crypto_api.h \
- /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdbool.h
--- build/CMakeFiles/stm32_benchmark.elf.dir/platforms/stm32f411_nucleo/runtime_overhead_probe.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/stm32_benchmark.elf.dir/platforms/stm32f411_nucleo/runtime_overhead_probe.c.obj: \
- /home/piec/Documents/ResearchInternship/cryptoBenchmark/platforms/stm32f411_nucleo/runtime_overhead_probe.c
